6b0cbcfbaed3bff70eab1a00a7d2ee6423048184
Gabighz <ghiuzangabriel2@gmail.com>
2018-11-18 22:58:07 +0000

    Initial commit for Administrator class

---
+++ src/Administrator.java
@@ -0,0 +1,28 @@
+/**
+ * This class constructs an Administrator object.
+ *
+ * It can add and remove user accounts, granting suitable privileges to
+ * users to perform only the designated tasks for their role.
+ *
+ * It can add and remove university departments from the system.
+ *
+ * It can add and remove degree courses, linking them to the one or many departments that teach
+ * the degree, indicating the lead department for the degree.
+ *
+ * It can add and remove modules, linking them to the degrees and levels of study for which they are
+ * approved (stating whether core or not).
+ *
+ * @author Gabriel Ghiuzan
+ */
+
+public class Administrator {
+
+    /**
+     * Constructs an Administrator
+     */
+    public Administrator () {
+
+
+    }
+
+}
