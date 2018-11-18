233841bffa4f9792d0f3115993b004278aacf8aa
Gabighz <ghiuzangabriel2@gmail.com>
2018-11-18 23:02:04 +0000

    Initial commit for Account class

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+/**
+ * This class constructs an Account object.
+ *
+ * It has get methods for title, forename, and name.
+ * It has no set methods, given that the data stored in the object shouldn't be changed.
+ *
+ * @author Gabriel Ghiuzan
+ */
+
+public class Account {
+
+    /**
+     * Constructs an Account
+     */
+    public Account () {
+
+
+    }
+
+}
