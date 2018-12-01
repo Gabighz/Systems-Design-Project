44992384f30885fe5489601873efd4419f4f96c3
Gabighz <ghiuzangabriel2@gmail.com>
2018-12-01 21:01:53 +0000

    Added a Main class and logIn function

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.sql.*;
+
+/**
+ * This class handles log-in and *to be completed*
+ *
+ * @author Gabriel Ghiuzan
+ */
+
+public class Main {
+    /**
+     * Takes a user email address and password as input.
+     * Then it checks this data against the Account table.
+     *
+     * Hashing should be deterministic. Therefore, if the passwords match and the same hashing is used,
+     * then their hashed veresultSetions should match.
+     *
+     * @param emailAddress       The given email address for logging in.
+     * @param passwordToHash     The corresponding password for logging in.
+     */
+    public void logIn(String emailAddress, String passwordToHash) {
+
+        String DB = "jdbc:mysql://stusql.dcs.shef.ac.uk/team030?user=team030&password=71142c41";
+        Statement statement = null;
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        MessageDigest md = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+            md.update(salt);
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("SHA-512 is not a valid message digest algorithm");
+
+        }
+
+        byte[] hashedPassword = null;
+
+        try {
+            hashedPassword = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+
+        } catch (NullPointerException e) {
+            System.err.println("Digest is a null pointer.");
+
+        }
+
+        try (Connection con = DriverManager.getConnection(DB)) {
+            statement = con.createStatement();
+            String toExecute = String.format("SELECT * FROM Accounts WHERE Email='%s'", emailAddress);
+            ResultSet resultSet = statement.executeQuery(toExecute);
+
+            if (resultSet == null) {
+                System.out.println("This email address is not registered.");
+
+            } else {
+
+                String email = null;
+                String password = null;
+                String role = null;
+
+                while (resultSet.next())
+                {
+                    email = resultSet.getString("Email");
+                    password = resultSet.getString("Password");
+                    role = resultSet.getString("Role");
+                }
+
+                if (password.equals(hashedPassword.toString())) {
+                    System.out.println("Log-in successful!");
+                } else {
+                    System.out.println("Wrong password");
+                }
+
+            }
+
+            statement.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
