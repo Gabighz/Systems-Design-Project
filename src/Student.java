83e4daa26e59c42de13bef78e3b8e53368230f3c
asmith36 <asmith36@sheffield.ac.uk>
2018-12-05 07:53:23 +0000

    correcting student file

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
 * Class for a student object
 *
 * @author Amy Smith
@@ -73,34 +72,4 @@ public class Student {
       return student;
   }
   
-
-=======
- * This class constructs a Student object.
- *
- * @author Alexandre Gauthier
- */
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Student {
-
-    final static String DB = "jdbc:mysql://stusql.dcs.shef.ac.uk/team030?user=team030&password=71142c41";
-
-    /**
-     * View student status.
-     *
-     * @param regNo The student registration number.
-     * @returns The student status.
-     */
-    public static String get(int regNo) {
-        return "1";
-    }
-
-
-
-    public static void main(String[] args) {
-    }
->>>>>>> c81a4f5dfcf6d18f8331938e3c39f82400244b62
 }
